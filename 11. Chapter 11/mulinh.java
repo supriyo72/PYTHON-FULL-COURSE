9dca791c5eb0ced12b3c4c7ecc64b8dccaff1131
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-09-27 08:08:50 +0530

    Add files via upload

---
+++ 11. Chapter 11/mulinh.java
@@ -0,0 +1,28 @@
+Class X
+{
+   public void methodX()
+   {
+     System.out.println("Class X method");
+   }
+}
+Class Y extends X
+{
+public void methodY()
+{
+System.out.println("class Y method");
+}
+}
+Class Z extends Y
+{
+   public void methodZ()
+   {
+     System.out.println("class Z method");
+   }
+   public static void main(String args[])
+   {
+     Z obj = new Z();
+     obj.methodX(); //calling grand parent class method
+     obj.methodY(); //calling parent class method
+     obj.methodZ(); //calling local method
+  }
+}
